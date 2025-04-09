d15a857a5448b34a9390545d7e60f9de9c34eadb
Monique Duarte <moniquead95@gmail.com>
2025-04-09 13:51:49 -0300

    preparação para a logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,14 @@
+// import logo from "../../assets/techzoow.png";
 const Header = () => {
   return (
     <header className="text-center text-white w-full bg-[#005b8f]">
+      {/* <div className="flex">
+        <img className="w-[10vw]" src={logo} alt="TechZoow Logo"></img> */}
+      <div className="block">
       <h1>TechZoow Consultoria </h1>
       <h2>Consultoria em tecnologia</h2>
+      </div>
+      {/* </div> */}
     </header>
   );
 }
