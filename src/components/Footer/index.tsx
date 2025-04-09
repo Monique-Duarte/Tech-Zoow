0c085e6c295eb1f9e6963963f714ff8b789d9224
Monique Duarte <moniquead95@gmail.com>
2025-04-09 13:39:16 -0300

    feat: atualiza estilo do componente Footer para fundo azul

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 text-center w-full bg-[#fff]">
+    <footer className=" text-white fixed bottom-0 text-center w-full bg-[#005b8f]">
       © 2025 Todos os direitos reservados.
     </footer>
   );
